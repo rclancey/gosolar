f34f340f2fe72478b05aa133efff1b1c04ff0633
Ryan Clancey <rclancey@gmail.com>
2023-11-26 15:38:54 -0800

    updates leap seconds to 2023

--- time.go
+++ time.go
@@ -69,6 +69,8 @@ var LeapSecondsAdjustments = [][2]int{
 	[2]int{0, 0}, // 2019
 	[2]int{0, 0}, // 2020
 	[2]int{0, 0}, // 2021
+	[2]int{0, 0}, // 2022
+	[2]int{0, 0}, // 202r
 }
 
 func GetLeapSeconds(when time.Time) int {
